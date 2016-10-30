aa1fd89989ddb20d55f10adf86dea436db14086e
Guneet Sachdeva <guneetssachdeva@gmail.com>
2016-10-30 10:12:49 -0700

    made minor changes

--- src/java/org/guneet/Hello.java
+++ src/java/org/guneet/Hello.java
@@ -3,6 +3,7 @@ package org.guneet;
 public class Hello {
 
    public static void main(String[] args) {
-      System.out.println("Hello World");
+      System.out.println("Hello Guneet!");
+
    }
 }
