27e1d6e233e26d96102135e8e01952af445bc170
Guneet Sachdeva <guneetssachdeva@gmail.com>
2016-10-30 10:39:24 -0700

    made minor changes

--- src/java/org/guneet/AddNumbers.java
+++ src/java/org/guneet/AddNumbers.java
@@ -3,7 +3,7 @@
  */
 public class AddNumbers {
     public static void main(String[] args){
-        int x = 3;
+        int x = 3;  // guneet
         int y = 7;
         System.out.println(x + " + " + y + " = " + (x+y));
     }
