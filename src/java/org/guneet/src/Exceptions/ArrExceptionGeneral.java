320cb7b3569b59ec0351b50a7a181b0e56bbe837
Guneet Sachdeva <guneetssachdeva@gmail.com>
2017-02-12 10:20:43 -0800

    Testing git

---
+++ src/java/org/guneet/src/Exceptions/ArrExceptionGeneral.java
@@ -0,0 +1,26 @@
+package src.Exceptions;
+
+/**
+ * Created by guneetsachdeva on 2/12/17.
+ */
+public class ArrExceptionGeneral {
+    public static void main (String []args){
+        int[] someArr= new int [10];
+
+        try {
+
+
+            someArr[1] = 4;
+            someArr[3] = 7;
+            someArr[5] = 2;
+            someArr[10] = 5;//program died here
+        } catch (Exception e){ // catch all exceptions
+            System.out.println("Something went wrong: " + e.getMessage());
+            
+        }
+
+        for(int i=0;i<10;i++){ // never reaches this line
+            System.out.println(someArr[i]);
+        }
+    }
+}
