6aff286c385be5b5be54ef38e59a9e5fe8191a25
Guneet Sachdeva <guneetssachdeva@gmail.com>
2017-02-19 10:40:01 -0800

    Testing git

---
+++ src/java/org/guneet/src/Operators/OperatorLoop.java
@@ -0,0 +1,18 @@
+package src.Operators;
+
+/**
+ * Created by guneetsachdeva on 2/19/17.
+ */
+public class OperatorLoop {
+    public static void main (String [] args){
+
+        int z =0;
+        int i;
+
+        for(i =0; i<4; i++){
+            z++;
+            System.out.println(" i = " + i +", z = " +z);
+        }
+        System.out.println(" i = " + i +", z = " +z);
+    }
+}
